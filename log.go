5b7cbfd510533ac59a317abd6ef4716a4927d06e
徐乔伦 <xql@feiniubus.com>
2017-10-28 15:00:39 +0800

    封装zap logger

--- log.go
+++ log.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"fmt"
 	"log/syslog"
 	"os"
 	"runtime"
@@ -10,11 +11,15 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+type Logger struct {
+	log *zap.Logger
+}
+
 func CallerEncoder(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(strings.Join([]string{caller.TrimmedPath(), runtime.FuncForPC(caller.PC).Name()}, ":"))
 }
 
-func New(debugLevel bool) (*zap.Logger, error) {
+func New(debugLevel bool) (*Logger, error) {
 	config := zap.NewProductionConfig()
 	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	config.EncoderConfig.EncodeCaller = func(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
@@ -25,14 +30,19 @@ func New(debugLevel bool) (*zap.Logger, error) {
 		config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
 	}
 
-	return config.Build()
+	_log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Logger{log: _log}, nil
 }
 
-func NewLogstash(debugLevel bool, host string, port int) (*zap.Logger, error) {
+func NewLogstash(debugLevel bool, host string, port int) (*Logger, error) {
 	return NewLogstashWithTimeout(debugLevel, host, port, 10)
 }
 
-func NewLogstashWithTimeout(debugLevel bool, host string, port int, timeout int) (*zap.Logger, error) {
+func NewLogstashWithTimeout(debugLevel bool, host string, port int, timeout int) (*Logger, error) {
 	cfg := zap.NewProductionEncoderConfig()
 	cfg.EncodeCaller = CallerEncoder
 	cfg.EncodeTime = zapcore.ISO8601TimeEncoder
@@ -54,11 +64,11 @@ func NewLogstashWithTimeout(debugLevel bool, host string, port int, timeout int)
 
 	zap.NewProductionConfig()
 
-	logger := zap.New(zapcore.NewCore(enc, zapcore.Lock(sink), atom))
-	return logger, nil
+	_log := zap.New(zapcore.NewCore(enc, zapcore.Lock(sink), atom))
+	return &Logger{log: _log}, nil
 }
 
-func NewSyslog(debugLevel bool, app string) (*zap.Logger, error) {
+func NewSyslog(debugLevel bool, app string) (*Logger, error) {
 	enc := NewSyslogEncoder(SyslogEncoderConfig{
 		EncoderConfig: zapcore.EncoderConfig{
 			NameKey:        "logger",
@@ -89,5 +99,94 @@ func NewSyslog(debugLevel bool, app string) (*zap.Logger, error) {
 		atom = zap.NewAtomicLevelAt(zap.InfoLevel)
 	}
 
-	return zap.New(zapcore.NewCore(enc, zapcore.Lock(sink), atom)), nil
+	_log := zap.New(zapcore.NewCore(enc, zapcore.Lock(sink), atom))
+	return &Logger{log: _log}, nil
+}
+
+func (l *Logger) Print(v ...interface{}) {
+	l.log.Info(fmt.Sprint(v))
+}
+
+func (l *Logger) Printf(format string, args ...interface{}) {
+	l.log.Info(fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) Println(v ...interface{}) {
+	l.log.Info(fmt.Sprintln(v))
+}
+
+func (l *Logger) Debug(v ...interface{}) {
+	l.log.Debug(fmt.Sprint(v))
+}
+
+func (l *Logger) Debugf(format string, args ...interface{}) {
+	l.log.Debug(fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) Debugln(v ...interface{}) {
+	l.log.Debug(fmt.Sprintln(v))
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	l.log.Info(fmt.Sprint(v))
+}
+
+func (l *Logger) Infof(format string, args ...interface{}) {
+	l.log.Info(fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) Infoln(v ...interface{}) {
+	l.log.Info(fmt.Sprintln(v))
+}
+
+func (l *Logger) Warn(v ...interface{}) {
+	l.log.Warn(fmt.Sprint(v))
+}
+
+func (l *Logger) Warnf(format string, args ...interface{}) {
+	l.log.Warn(fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) Warnln(v ...interface{}) {
+	l.log.Warn(fmt.Sprintln(v))
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	l.log.Error(fmt.Sprint(v))
+}
+
+func (l *Logger) Errorf(format string, args ...interface{}) {
+	l.log.Error(fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) Errorln(v ...interface{}) {
+	l.log.Error(fmt.Sprintln(v))
+}
+
+func (l *Logger) Fatal(v ...interface{}) {
+	l.log.Fatal(fmt.Sprint(v))
+}
+
+func (l *Logger) Fatalf(format string, args ...interface{}) {
+	l.log.Fatal(fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) Fatalln(v ...interface{}) {
+	l.log.Fatal(fmt.Sprintln(v))
+}
+
+func (l *Logger) Panic(v ...interface{}) {
+	l.log.Panic(fmt.Sprint(v))
+}
+
+func (l *Logger) Panicf(format string, args ...interface{}) {
+	l.log.Panic(fmt.Sprintf(format, args...))
+}
+
+func (l *Logger) Panicln(v ...interface{}) {
+	l.log.Panic(fmt.Sprintln(v))
+}
+
+func (l *Logger) With(key string, value interface{}) *Logger {
+	return &Logger{l.log.With(zap.Any(key, value))}
 }
